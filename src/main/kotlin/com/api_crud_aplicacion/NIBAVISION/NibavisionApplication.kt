a5153be12914dd7d1dcd50228fe61086f75ee6c0
Bastian <ba.rubiom@duocuc.cl>
2025-11-11 13:24:46 -0300

    Primer commit de la API de Spring Boot

---
+++ src/main/kotlin/com/api_crud_aplicacion/NIBAVISION/NibavisionApplication.kt
@@ -0,0 +1,11 @@
+package com.api_crud_aplicacion.NIBAVISION
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NibavisionApplication
+
+fun main(args: Array<String>) {
+	runApplication<NibavisionApplication>(*args)
+}
---
+++ src/test/kotlin/com/api_crud_aplicacion/NIBAVISION/NibavisionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api_crud_aplicacion.NIBAVISION
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NibavisionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
